2363a3f608fbf621e24fd6ca4cdbb98b7bf98385
vinodasha2007-oss <vino.dasha.2007@gmail.com>
2026-05-05 01:00:35 +0300

    Update load_rocket.py

--- load_rocket.py
+++ load_rocket.py
@@ -1,11 +1,34 @@
-
 import pybullet as p
 import pybullet_data
 import numpy as np
-from typing import Optional, Tuple, Any
-from load_rocket import RocketLoader
+from typing import Optional, Tuple
+
+
+class RocketLoader:
+    """Загрузчик URDF моделей ракеты"""
+
+    def __init__(self, client):
+        self.client = client
+
+    def load(self, urdf_path: str, base_position=None, base_orientation=None) -> int:
+        """Загрузка URDF файла"""
+        if base_position is None:
+            base_position = [0, 0, 0]
+        if base_orientation is None:
+            base_orientation = [0, 0, 0, 1]
+
+        rocket_id = p.loadURDF(
+            urdf_path,
+            base_position,
+            base_orientation,
+            useFixedBase=False,
+            physicsClientId=self.client
+        )
+        return rocket_id
+
 
 class PyBulletSimulation:
+    """Альтернативный класс симуляции (для совместимости)"""
 
     def __init__(self, gui: bool = True, debug: bool = False):
         self.debug = debug
@@ -14,6 +37,7 @@ class PyBulletSimulation:
         self.ground_id: Optional[int] = None
 
     def setup_environment(self, urdf_path: str = "rocket.urdf"):
+        """Настройка окружения"""
         from cfg import Simcfg
 
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -24,6 +48,7 @@ class PyBulletSimulation:
         self.rocket_id = loader.load(urdf_path)
 
     def get_state(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """Получение текущего состояния ракеты"""
         if self.rocket_id is None:
             raise RuntimeError("Ракета не загружена")
 
@@ -37,20 +62,28 @@ class PyBulletSimulation:
             np.array(angular_vel)
         )
 
-    def apply_force(self, force: np.ndarray):
+    def set_position(self, position: list, orientation: list = [0, 0, 0, 1]):
+        """Установка позиции ракеты"""
+        if self.rocket_id is not None:
+            p.resetBasePositionAndOrientation(self.rocket_id, position, orientation)
+
+    def set_velocity(self, linear_vel: list = [0, 0, 0], angular_vel: list = [0, 0, 0]):
+        """Установка скорости ракеты"""
+        if self.rocket_id is not None:
+            p.resetBaseVelocity(self.rocket_id, linear_vel, angular_vel)
+
+    def apply_force(self, force: np.ndarray, position: list = [0, 0, 0], frame: int = p.WORLD_FRAME):
+        """Приложение внешней силы"""
         if self.rocket_id is None:
             raise RuntimeError("Ракета не загружена")
 
-        p.applyExternalForce(
-            self.rocket_id,
-            -1,
-            force,
-            [0, 0, 0],
-            p.WORLD_FRAME
-        )
+        p.applyExternalForce(self.rocket_id, -1, force, position, frame)
 
     def step(self):
+        """Шаг симуляции"""
         p.stepSimulation()
 
     def dispose(self):
-        p.disconnect()
+        """Отключение от сервера"""
+        if self.client is not None:
+            p.disconnect(self.client)
